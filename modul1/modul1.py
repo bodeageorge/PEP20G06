82e67bd37deac9ab50abf7a210ea2a191d9c931e
gbodea <bodea.george.danut@gmail.com>
2020-11-18 21:35:46 +0200

    modul1

---
+++ modul1/modul1.py
@@ -0,0 +1,22 @@
+print('hello world')
+print('hello world', 'hello world', 'hello world', sep='- -')
+print('hello world', end='$\n')
+
+hello = 'hello world'
+print(hello)
+print(print)
+
+print('*Bodea George*')
+
+number1 = 1
+number2 = 2
+number3 = number1 - number2
+print(number3)
+
+number1 = 1
+number2 = 2
+number3 = number1 / number2
+print(number3)
+
+
+print(number1.__add__(2))
